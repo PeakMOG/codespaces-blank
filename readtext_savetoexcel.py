446e9a7a43892082f8561f966e2b109ea7913d12
PeakMOG <126115416+PeakMOG@users.noreply.github.com>
2023-02-23 16:50:54 +0200

    Update readtext_savetoexcel.py

--- readtext_savetoexcel.py
+++ readtext_savetoexcel.py
@@ -1,39 +1,39 @@
 import re
 import openpyxl
-
-
+from filereaderclasses import Abonament
 
 def search_txt():
     file = open("text-reader.txt", "r")
     text = file.read()
-    word1 = re.compile(r"Business\s+(\w+\s+\w+)")
-    word2 = re.compile(r"Termen plata:\s+(\d{2}[/-]\d{2}[/-]\d{4})")
+    #regex compile - data \d{2}[/-]\d{2}[/-]\d{4}
+    denumire = re.compile(r"Business\s+(\w+\s+\w+)")
+    scadenta = re.compile(r"Termen plata:\s+(\d{2}[/-]\d{2}[/-]\d{4})")
     word3 = re.compile(r"buc. 1\s+(\w+)")
 
+    a1 = Abonament(word1, 365, word3, word3, word2)
+
+    #1
     match1 = word1.search(text)
     if match1:
         next2w1 = match1.group(1)
     else:
         next2w1 = None
-    print(next2w1, word1)
-    # print("Next two words after word1: ", next2w1)
 
+    #2
     match2 = word2.search(text)
     if match2:
         next2w2 = match2.group(1)
     else:
         next2w2 = None
-    print(next2w2, word2)
-    # print("Next two words after word1: ", next2w1)
 
+    #3
     match3 = word3.search(text)
     if match3:
         next2w3 = match3.group(1)
     else:
         next2w3 = None
-    print(next2w3, word3)
-    # print("Next two words after word1: ", next2w1)
 
+    #text-to-excel
     wb = openpyxl.load_workbook("ff.xlsx")
     ws = wb.active
     last_row = ws.max_row + 1
@@ -44,10 +44,7 @@ def search_txt():
         #col3 = i+3
 
         ws.cell(row=last_row, column=1, value=words[0])
-        ws.cell(row=last_row, column=4, value=words[1] if col1 < len(words) else None)
-        ws.cell(row=last_row, column=3, value=words[2] if col2 < len(words) else None)
+        ws.cell(row=last_row, column=4, value=words[1])
+        ws.cell(row=last_row, column=3, value=words[2])
         ws.cell(row=last_row, column=2, value="=today()+365")
-        last_row += 1
     wb.save("ff.xlsx")
-
-search_txt()
